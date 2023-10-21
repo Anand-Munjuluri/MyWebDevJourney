42424e1b6c264e5b54bb26fb7cd801a2f16cbcc5
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2023-10-22 00:00:38 +0530

    Component-Composition

--- React/1-Inception/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const heading= React.createElement("h1",{id: "heading"},"Hello World from React!");
-    const root= ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading); 
-    //will replace whatever is there in html file, and convert object to tags,and REPLACE heading
