6720afedd87676ab4e29935e254c5e500fb68d29
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2023-10-23 22:15:12 +0530

    update with state variable
    create a function to display top rated restaurants on a click of a button

--- React/Lets-get-hooked/src/components/Body.js
+++ React/Lets-get-hooked/src/components/Body.js
@@ -1,17 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import ResList from "../utils/mockData";
+import { useState } from "react";
+
+
 const Body = () => {
+    const[ListofRes,setListofRes]= useState(ResList);
     return(
         <div className="body">
-            <div className="search">Search</div>
-            <div className="res-container" >
-                {
-                    ResList.map(restaurant => <RestaurantCard key={restaurant.info.id} ResData={restaurant}/>) 
-                }
-               
-                
-            </div>
+        <div className="filter">
+            <button className="filter-btn" onClick={() => {
+                const filteredList = ListofRes.filter((res) => res.info.avgRating > 4);
+                setListofRes(filteredList);
+                                                          }
+                                                   }
+            >
+                Top Rated Restaurant
+            </button>
         </div>
+        <div className="res-container">
+            {
+                    ListofRes.map(restaurant => <RestaurantCard key={restaurant.info.id} ResData={restaurant}/>)
+            }
+        </div>
+    </div>
+    
     )
     }
 
