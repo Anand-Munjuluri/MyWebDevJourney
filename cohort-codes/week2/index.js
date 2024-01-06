efc1bd4e78e5ac3fb1de2996430161e3fd19c623
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2024-01-06 20:01:51 +0530

    update excalidraw notes

--- cohort-codes/week2/index.js
+++ cohort-codes/week2/index.js
@@ -1,17 +1,12 @@
 //hello word with express
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 const bodyParser = require('body-parser');
-const fs = require('fs');
-fs.readFile("a.txt", "utf8", function(err, data)
-{
-
-})
-
+           
 app.use(bodyParser.json());
 app.post('/', function(req, res){
-    console.log(req.body);
+    console.log(req.query.message);
     res.send('<h1>Hello World!</h1>');
 }
 );
