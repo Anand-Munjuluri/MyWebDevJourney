4b156b3636cab7a90d70100e9f9eff2b62a42177
Anand Munjuluri <100225249+Anand-Munjuluri@users.noreply.github.com>
2024-01-20 22:59:05 +0530

    nested react elements

--- React-restart/01-Inception/App.js
+++ React-restart/01-Inception/App.js
@@ -1,11 +1,17 @@
   //create h1 tag with createElement
-  const heading = React.createElement('h1',{
-    id: "main-heading",
-    xyz: "abc" //attributes to h1 tag
-  },"Hello world from React");
-
+const parent = React.createElement("div", {id:"parent"},[
+React.createElement("div", {id:"child"},[
+  React.createElement("h1",{},"I am h1"),
+  React.createElement("h1",{},"I am a h2 tag")
+])
+],
+[React.createElement("div", {id:"child2"},[
+  React.createElement("h1",{},"I am h1"),
+  React.createElement("h1",{},"I am a h2 tag")
+])
+])
   //create root
   const root = ReactDOM.createRoot(document.getElementById('root'));
 
   //render the heading into html
-  root.render(heading);
+  root.render(parent);
